d0b1fd3386320ece35ab86e6d30055848e121713
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-10-10 10:37:08 +0900

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root to: "home#authentication"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  get 'home/authentication'
+
+  devise_for :users
 end
