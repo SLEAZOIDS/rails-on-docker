1c5697cac4cf8252944fa767db2c8c948255573c
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-10-11 15:09:13 +0900

    ログイン前後でのアクセステスト実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "home#authentication"
+  root 'home#authentication'
 
   get 'home/index'
   get 'home/authentication'
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,12 +1,20 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
     get home_index_url
     assert_response :success
   end
 
-  test "should get authentication" do
+  test "should get authentication 未ログイン->リダイレクト" do
+    get home_authentication_url
+    assert_redirected_to '/users/sign_in'
+  end
+
+  test "should get authentication ログイン" do
+    sign_in(users(:one))
     get home_authentication_url
     assert_response :success
   end
