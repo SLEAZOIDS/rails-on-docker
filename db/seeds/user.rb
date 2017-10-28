5c8e44afab880af651d63651ad8da7f94f501cec
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-10-28 14:42:57 +0900

    管理ユーザーをseedに追加

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,3 +1,5 @@
+User.connection.execute("TRUNCATE TABLE users;")
+
 max = 100
 1.upto(max) do |i|
   user = User.new(
@@ -9,4 +11,14 @@ max = 100
   user.save!
   print "\r#{i}/#{max}"
 end
+
+user = User.new(
+  :email => "common_admin@example.com",
+  :password => "common_admin",
+  :password_confirmation => "common_admin",
+  :admin_flg => true
+)
+user.skip_confirmation!
+user.save!
+
 puts " End"
