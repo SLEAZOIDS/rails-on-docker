7e5fbf76cbf6ac97a31f8da43ec53f39b0765ff6
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-10-29 15:59:56 +0900

    [modify]パスワードを楽に

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -3,9 +3,9 @@ User.connection.execute("TRUNCATE TABLE users;")
 max = 100
 1.upto(max) do |i|
   user = User.new(
-    :email => "common_user#{i}@example.com",
-    :password => "common_user#{i}",
-    :password_confirmation => "common_user#{i}"
+    :email => "user#{i}@example.com",
+    :password => "aaaaaa",
+    :password_confirmation => "aaaaaa"
   )
   user.skip_confirmation!
   user.save!
@@ -13,9 +13,9 @@ max = 100
 end
 
 user = User.new(
-  :email => "common_admin@example.com",
-  :password => "common_admin",
-  :password_confirmation => "common_admin",
+  :email => "admin@example.com",
+  :password => "aaaaaa",
+  :password_confirmation => "aaaaaa",
   :admin_flg => true
 )
 user.skip_confirmation!
