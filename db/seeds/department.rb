1d8d10b3c2365223daab2864d37690757c5dfc5b
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-10-29 14:32:42 +0900

    create scatffold department

---
+++ db/seeds/department.rb
@@ -0,0 +1,9 @@
+Department.connection.execute("TRUNCATE TABLE departments;")
+
+Department.create(name: "総務")
+Department.create(name: "経理")
+Department.create(name: "営業第一課")
+Department.create(name: "営業第二課")
+Department.create(name: "営業第三課")
+Department.create(name: "webサービス開発部")
+Department.create(name: "システムソリューション開発部")
